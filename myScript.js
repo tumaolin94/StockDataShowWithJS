10f0247a6d9586d2061d453b6aff0b7d94667b74
tumaolin94 <524561223@qq.com>
2017-10-21 00:38:31 -0700

    initialize search form

---
+++ myScript.js
@@ -0,0 +1,10 @@
+/*
+ @author: Maolin Tu
+ @Blog: http://tumaolin.com 
+*/
+
+
+function formReset(){
+    document.getElementById("form1").reset();
+    document.getElementById('whole_container').innerHTML = "";
+}
