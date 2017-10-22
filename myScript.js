8fbae71b20a60c5f1c752eac0de3d5df6777906a
tumaolin94 <524561223@qq.com>
2017-10-22 00:15:21 -0700

    add information table, Price chart

--- myScript.js
+++ myScript.js
@@ -7,4 +7,6 @@
 function formReset(){
     document.getElementById("form1").reset();
     document.getElementById('whole_container').innerHTML = "";
+    document.getElementById('container').innerHTML = "";
+    document.getElementById('container').style.visibility='hidden';
 }
